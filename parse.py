108ec46317b782454ae80dde15510a73f1ddadae
Lukas Kretschmar <lukaskretschmar1@gmail.com>
2025-01-21 18:02:49 +0100

    Hotfix for parser debug log

--- parse.py
+++ parse.py
@@ -7,17 +7,27 @@ def main(args: list[str]) -> int:
         print(f"[{time.strftime('%F %T')}] No Documents to parse")
         return 0
     
+    existant_documents = []
     documents_metadata = {}
 
-    print(f"[{time.strftime('%F %T')}] Working through input directory...")
-    for document_path in os.listdir("btd"):
-        if not str.endswith(document_path,".pdf"): continue
+    for file in os.listdir("btd"):
+        if not str.endswith(file,".pdf"): continue
+        existant_documents.append(file)
+    total_document_count = len(existant_documents)
 
-        print(f"[{time.strftime('%F %T')}] Reading document: {document_path}")
+    print(f"[{time.strftime('%F %T')}] Working through input directory...")
+
+    current_document_index = 1
+    for document_path in existant_documents:
+        completion_percentage = (float(current_document_index) / float(total_document_count)) * 100.0
+        print(f"[{time.strftime('%F %T')}] {completion_percentage :.2f}% - Reading document: {document_path}")
+        
         with open(f"btd/{document_path}", "rb") as document_handle:
             document_reader = pypdf.PdfReader(document_handle)
             documents_metadata[document_path] = dict(document_reader.metadata)
             document_handle.close()
+        current_document_index += 1
+
     with open("btd/metadata.json", "w") as metadata_handle:
         json.dump(documents_metadata, metadata_handle, indent=4)
         metadata_handle.close()
