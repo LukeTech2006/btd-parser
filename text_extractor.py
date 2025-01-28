bbec80354c96d1833772779e93314f7f6162b59c
LukeTech2006 <lukaskretschmar1@gmail.com>
2025-01-28 07:47:14 +0100

    Beta Version of text extractor

--- text_extractor.py
+++ text_extractor.py
@@ -9,18 +9,38 @@ def main(args: list) -> int:
         if file.endswith(".pdf"): documents.append(file)
     documents.sort()
 
-    persist_line = False
+    skip_next = False
+    parsed_lines = []
     for document in documents:
-        with open("btd/" + document, "rb") as document_handle:
+        with open("btd/"+document, "rb") as document_handle:
             document_pdf = pypdf.PdfReader(document_handle)
-            pages_text = [page.extract_text() for page in document_pdf.pages]
-            for page_text in pages_text:
-                for line_text in page_text.splitlines():
-                    if line_text.strip() == "": continue
+            document_pages = [page.extract_text() for page in document_pdf.pages]
+
+            for i in range(document_pages.__len__()):
+                document_page_lines = document_pages[i].splitlines()
+                for j in range(document_page_lines.__len__()):
+                    line = document_page_lines[j]
+
+                    if skip_next:
+                        skip_next = False
+                        continue
+
+                    if line.strip() == "": continue
+                    line = line.strip()
+                    
                     try:
-                        if line_text[-1] == "-" and not line_text[-2].isspace(): print("Zeilentrenner!", document)
+                        print("Zeile:", j, line[-1], line[-2])
+                        if line[-1] == "-" and (not line[-2].isspace()):
+                            parsed_lines.append(line[:-1] + document_page_lines[j+1])
+                            skip_next = True
+                            print("Zeilentrenner in Zeile", j)
+                            continue
                     except: pass
-                    #print(line_text)
+
+                    parsed_lines.append(line)
+
+        with open("btd/text/"+document[:-4]+".txt", "w") as text_file_handle:
+            text_file_handle.write("\n".join(parsed_lines))
 
     return 0
 
